6e17d8836a3ba2829c4f3468b80747764cc04b78
Gabby <gabzo24@live.com.au>
2023-12-09 14:27:40 +1100

    drawing canvas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 /*----- constants -----*/
 
 //constants of player, obstacles, score counter, game canvas
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
+ctx.moveTo(0, 0);
+ctx.lineTo(801, 601) //account for 1px border for edges
+ctx.stroke();
 /*----- state variables -----*/
 
 //position, size and speed of player and obstacles
@@ -15,6 +20,17 @@
 //keypress listener for keys that allow up and down movement for player.
 
 /*----- functions -----*/
+
+//checking the positioning of objects
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    console.log("x: " + x + " y: " + y)
+}
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+})
 
 //function drawPlayer()
 // Draw the player on the canvas
