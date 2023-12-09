6b353dc56c9bd001ad56816569096be45a089ec4
Gabby <gabzo24@live.com.au>
2023-12-09 16:23:19 +1100

    trying to move player

--- js/main.js
+++ js/main.js
@@ -4,10 +4,26 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.moveTo(0, 0);
-ctx.lineTo(801, 601) //account for 1px border for edges
-ctx.stroke();
+const player = {
+    x: 0,
+    y: canvas.height / 2,
+    width: 101,
+    height: 51,
+    color: "red",
+    speed: 10,
+};
+
+
+let score = 0;
+
+// ctx.moveTo(0, 0);
+// ctx.lineTo(801, 601) //account for 1px border for edges
+// ctx.stroke();
+
 /*----- state variables -----*/
+
+let movingUp = false;
+let movingDown = false;
 
 //position, size and speed of player and obstacles
 //obstacle creation rate
@@ -16,6 +32,15 @@ ctx.stroke();
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
+
+document.addEventListener("keydown", function(evt){
+    handleKeyPress(evt.key, true);
+    // console.log(evt.code)
+});
+
+document.addEventListener("keyup", function(evt) {
+    handleKeyPress(evt.key, false);
+});
 
 //keypress listener for keys that allow up and down movement for player.
 
@@ -32,8 +57,12 @@ canvas.addEventListener('mousedown', function(e) {
     getCursorPosition(canvas, e)
 })
 
-//function drawPlayer()
-// Draw the player on the canvas
+function drawPlayer(){
+    // Draw the player on the canvas
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x - player.width / 2, player.y - player.height / 2, player.width, player.height);
+    console.log("player")
+};
 
 //function drawObstacles()
 // Draw all obstacles on the canvas
@@ -41,11 +70,34 @@ canvas.addEventListener('mousedown', function(e) {
 //function drawScore()
 // Update and display the current score on the canvas
 
-//function updatePlayer()
+function handleKeyPress(key, isPressed){
+    movingUp = (key === "ArrowUp" || key === "w") && isPressed;
+    movingDown = (key === "ArrowDown" || key === "s") && isPressed;
+};
+
+function updatePlayer(){
 // Update the player's position and check for collisions with obstacles
+    if (movingUp){
+        player.y -= player.speed;
+    }else if (movingDown){
+        player.y += player.speed;
+    }
+};
 
 //function updateObstacles()
 // Update the obstacles' positions, create new obstacles, and check for collisions
 
-//function resetGame()
-// Reset the game state
+function init(){
+
+}
+
+function render(){
+
+    drawPlayer();
+    updatePlayer();
+    requestAnimationFrame(render)
+};
+
+
+init();
+render();
