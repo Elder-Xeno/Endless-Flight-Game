b1e178558dfc930e85d0c483d3af987c5e2d683c
Gabby <gabzo24@live.com.au>
2023-12-09 12:28:11 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/*----- constants -----*/
+
+//constants of player, obstacles, score counter, game canvas
+
+/*----- state variables -----*/
+
+//position, size and speed of player and obstacles
+//obstacle creation rate
+//how the score accumulates
+
+/*----- cached elements  -----*/
+
+/*----- event listeners -----*/
+
+//keypress listener for keys that allow up and down movement for player.
+
+/*----- functions -----*/
+
+//function drawPlayer()
+// Draw the player on the canvas
+
+//function drawObstacles()
+// Draw all obstacles on the canvas
+
+//function drawScore()
+// Update and display the current score on the canvas
+
+//function updatePlayer()
+// Update the player's position and check for collisions with obstacles
+
+//function updateObstacles()
+// Update the obstacles' positions, create new obstacles, and check for collisions
+
+//function resetGame()
+// Reset the game state
