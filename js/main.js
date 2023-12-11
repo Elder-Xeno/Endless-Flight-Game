83d8285c379bc3b026e7c1ca316b08dc5f55b4f6
Gabby <gabzo24@live.com.au>
2023-12-11 22:12:43 +1100

    working out logic to spawn multiple obstacles

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
 /*----- constants -----*/
 
-//constants of player, obstacles, score counter, game canvas
+// constants of player, obstacles, score counter, game canvas
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const player = {
-    x: 0,
-    y: canvas.height / 2,
-    width: 101,
-    height: 51,
-    color: "red",
-    speed: 10,
+  x: 0, // left and right
+  y: canvas.height / 2, // top and bottom
+  width: 101, // width and height are the size of the player
+  height: 51,
+  color: "red",
+  speed: 10,
 };
 
-const obstacle = {
-    x: canvas.width,
-    y: canvas.height / 2,
-    width: 60,
-    height: 50,
-    color: "blue",
-    speed: 5,
+const obstacleMid = {
+  x: canvas.width,
+  y: canvas.height / 2,
+  width: 60,
+  height: 60,
+  color: "blue",
+  speed: 10,
 };
 
-const obstacles = [];
+const obstacleTop = {
+  x: canvas.width,
+  y: 0,
+  width: 100,
+  height: 300,
+  color: "green",
+  speed: 3,
+};
+
+const obstacles = [obstacleMid, obstacleTop];
 
 let score = 0;
 
@@ -35,41 +44,30 @@ let score = 0;
 let movingUp = false;
 let movingDown = false;
 
-//position, size and speed of player and obstacles
-//obstacle creation rate
-//how the score accumulates
+let frameCounter = 0; // this will increment once per frame in the game loop to spawn obstacles and control their frequency.
+const spawnFrequency = 250;
+
+// position, size, and speed of player and obstacles
+// obstacle creation rate
+// how the score accumulates
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
-document.addEventListener("keydown", function(evt){
-    handleKeyPress(evt.key, true);
-    // console.log(evt.code)
+document.addEventListener("keydown", function (evt) {
+  handleKeyPress(evt.key, true);
+  // console.log(evt.code)
 });
 
-document.addEventListener("keyup", function(evt) {
-    handleKeyPress(evt.key, false);
+document.addEventListener("keyup", function (evt) {
+  handleKeyPress(evt.key, false);
 });
-//keypress listener for keys that allow up and down movement for player.
+// keypress listener for keys that allow up and down movement for the player.
 
 /*----- functions -----*/
 
-function init(){
-console.log("Game initialized.");
-    render();
-};
-
-function render(){
-    drawPlayer();
-    updatePlayer();
-    drawObstacle();
-    updateObstacle();
-//method tells the browser that I want to perform an animation. Makes animation smoother.
-    requestAnimationFrame(render)
-};
-
-//checking the positioning of objects
+// checking the positioning of objects
 // function getCursorPosition(canvas, event) {
 //     const rect = canvas.getBoundingClientRect()
 //     const x = event.clientX - rect.left
@@ -80,58 +78,129 @@ function render(){
 //     getCursorPosition(canvas, e)
 // });
 
-function drawPlayer(){
-// Clear the canvas using method clearRect() Updates player position
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-// Draw the player on the canvas
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.x - player.width / 2, player.y - player.height / 2, player.width, player.height);
-};
+function init() {
+  console.log("Game initialized.");
+  render();
+}
+
+function render() {
+  drawPlayer();
+  updatePlayer();
+  drawObstacle();
+  updateObstacle();
+  // method tells the browser that I want to perform an animation. Makes animation smoother.
+  requestAnimationFrame(render);
+}
+
+function drawPlayer() {
+  // Clear the canvas using method clearRect() Updates player position
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Draw the player on the canvas
+  ctx.fillStyle = player.color;
+  ctx.fillRect(
+    player.x - player.width / 2,
+    player.y - player.height / 2,
+    player.width,
+    player.height
+  );
+}
 
 function drawObstacle() {
-    ctx.fillStyle = obstacle.color;
-    ctx.fillRect(obstacle.x - obstacle.width / 2, obstacle.y - obstacle.height / 2, obstacle.width, obstacle.height);
+  obstacles.forEach((obs) => {
+    ctx.fillStyle = obs.color;
+    ctx.fillRect(
+      obs.x - obs.width / 2,
+      obs.y - obs.height / 2,
+      obs.width,
+      obs.height
+    );
+  });
 }
 
 // Function to update the obstacle's position
 function updateObstacle() {
-    obstacle.x -= obstacle.speed;
-// Check for collision with the player
-if (
-    player.x < obstacle.x + obstacle.width &&
-    player.x + player.width > obstacle.x &&
-    player.y < obstacle.y + obstacle.height &&
-    player.y + player.height > obstacle.y
-) {
-// Handle collision
-        console.log("Collision!");
-            alert("Game Over!");
+  obstacles.forEach((obs) => {
+    obs.x -= obs.speed;
 
-// Reset player and obstacle positions
-        player.x = 0;
-        player.y = canvas.height / 2;
-        obstacle.x = canvas.width;
-}
-// Reset obstacle position if it goes off the left side of the canvas
-    if (obstacle.x + obstacle.width/2 < 0) {
-        obstacle.x = canvas.width + obstacle.width / 2;
+    // Reset obstacle position if it goes off the left side of the canvas
+    if (obs.x + obs.width / 2 < 0) {
+      obs.x = canvas.width + obs.width / 2;
+      // Adjusts the height at which the obstacle will respawn
+      obs.y = Math.random() * (canvas.height - obs.height) + obs.height / 2;
     }
+  });
+
+  // Increase the frame counter
+  frameCounter++;
+
+   // Adjust the spawn frequency based on player speed
+   const adjustedSpawnFrequency =
+   spawnFrequency * (player.speed / 10);
+
+  // Spawn a new obstacle every spawnFrequency frames
+ if (frameCounter % adjustedSpawnFrequency === 0) {
+    obstacles.push({
+      ...obstacleTop,
+      x: canvas.width,
+      y: 0, // Always spawn from the top
+    });
+  }
+
+
+  // Reset frame counter to avoid large numbers
+  if (frameCounter > 1000) {
+    frameCounter = 0;
+  }
+
+  if (checkCollision()) {
+    console.log("Collision!");
+    alert("Game Over!");
+    // Reset player position
+    player.x = 0;
+    player.y = canvas.height / 2;
+    // Reset obstacles position
+    obstacles.forEach((obs) => {
+      obs.x = canvas.width;
+    });
+  }
 }
 
-function handleKeyPress(key, isPressed){
-    movingUp = (key === "ArrowUp" || key === "w") && isPressed;
-    movingDown = (key === "ArrowDown" || key === "s") && isPressed;
-};
+function handleKeyPress(key, isPressed) {
+  movingUp = (key === "ArrowUp" || key === "w") && isPressed;
+  movingDown = (key === "ArrowDown" || key === "s") && isPressed;
+}
 
-function updatePlayer(){
-// Update the player's position and check for collisions with obstacles
-    if (movingUp && player.y - player.height / 2 > 0){
-        player.y -= player.speed;
-    }else if (movingDown && player.y + player.height / 2 < canvas.height){
-        player.y += player.speed;
+function updatePlayer() {
+  // Update the player's position and check for collisions with obstacles
+  if (movingUp && player.y - player.height / 2 > 0) {
+    player.y -= player.speed;
+  } else if (movingDown && player.y + player.height / 2 < canvas.height) {
+    player.y += player.speed;
+  }
+}
+
+function checkCollision() {
+  // Check for collision with the player. /2 to consider half the width or height of each object when comparing
+  for (const obs of obstacles) {
+    if (
+      // Check if the right side of the player is greater than or equal to the left side of the obstacle.
+      player.x + player.width / 2.5 >= obs.x - obs.width / 2.5 &&
+      // Check if the left side of the player is less than or equal to the right side of the obstacle.
+      player.x - player.width / 2.5 <= obs.x + obs.width / 2.5 &&
+      // Check if the bottom side of the player is greater than or equal to the top side of the obstacle.
+      player.y + player.height / 2.5 >= obs.y - obs.height / 2.5 &&
+      // Check if the top side of the player is less than or equal to the bottom side of the obstacle.
+      player.y - player.height / 2.5 <= obs.y + obs.height / 2.5
+    ) {
+      // Adjust player position to avoid clipping through the obstacleTop
+      player.y = obs.y + obs.height / 2.5 + player.height / 2.5;
+      return true; // Collision detected with any obstacle
     }
-};
-//function drawScore()
+  }
+  return false; // No collision with any obstacle
+}
+
+// function drawScore()
 // Update and display the current score on the canvas
 
 init();
